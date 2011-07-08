db331a1ab263310827802f711a4ec9418aafe430
Shane R. Spencer <shane@bogomip.com>
2011-07-08 21:11:07 +0000

    Updated settings.py

--- django/stardasher/settings.py
+++ django/stardasher/settings.py
@@ -1,24 +1,44 @@
 # Django settings for stardasher project.
 
+import os
+import sys
+
+# Can't import django.utils.translation yet
+_ = lambda s: s
+
+STARDASHER_ROOT = os.path.abspath(os.path.split(__file__)[0])
+SITE_ROOT = '/'
+HTDOCS_ROOT = os.path.join(STARDASHER_ROOT, 'htdocs')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+  ('Shane R. Spencer', 'shane@bogomip.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'stardasher',                 # Or path to database file if using sqlite3.
+    'USER': 'stardasher',                 # Not used with sqlite3.
+    'PASSWORD': 'dashmaster7',            # Not used with sqlite3.
+    'HOST': 'bigboote',                   # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',                           # Set to empty string for default. Not used with sqlite3.
+  }
 }
+
+CACHES = {
+  'default': {
+    'BACKEND': 'redis_cache.RedisCache',
+    'LOCATION': 'bigboote',
+    'KEY_PREFIX': 'stardasher',
+  }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -27,7 +47,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -43,35 +63,37 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(HTDOCS_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = SITE_ROOT + 'media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(HTDOCS_ROOT, 'media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = MEDIA_URL + 'static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(STARDASHER_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -106,6 +128,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(STARDASHER_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -116,9 +139,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'django_extensions',
 )
 
 # A sample logging configuration. The only tangible logging
