f4e3ccfad105f4614d151abc69c27d12502a0a23
Shane R. Spencer <shane@bogomip.com>
2011-07-20 00:01:43 +0000

    model fixups.. making callerid redundant for query/search time (reducing joins).  It will never change.. ever.  Changed disposition to be an int.

--- django/stardasher/calls/models.py
+++ django/stardasher/calls/models.py
@@ -1,18 +1,10 @@
 import datetime
 
+from hashlib import sha256
+
 from django.db import models
 
 from django_extensions.db.fields import UUIDField
-
-class CallerID(models.Model):
-    name = models.CharField(max_length=80)
-    number = models.CharField(max_length=80)
-    ani = models.CharField(max_length=80)
-    rdnis = models.CharField(max_length=80)
-    dnid = models.CharField(max_length=80)
-    presentation_name = models.CharField(max_length=80)
-    presentation_number = models.CharField(max_length=80)
-    #parent information can be retrieved through linkedid in Call
 
 ### TODO: Add indexes later after query analysis
 class Batch(models.Model):
@@ -26,18 +18,14 @@ class Batch(models.Model):
 
     completed = models.BooleanField(default=0)
 
-class TagGroup(models.Model):
-    name = models.CharField(max_length=80) #Later used to deal with promotion codes and arbitrary data tagging.
+class TagHash(models.Model):
+    hash = models.TextField() #list representation of sorted list of tag names.. enables sorting... slow sorting which may want to be cached
 
 class Tag(models.Model):
-    name = models.CharField(max_length=80)
-    group = models.ManyToManyField(TagGroup)
+    name = models.CharField(max_length=1024)
+    hashs = models.ManyToManyField(TagHash)
 
-class TagHash(models.Model):
-    hash = models.CharField(max_length=8192, db_index=1) #list representation of sorted list of tag names.. allows sorting.
-    groups = models.ManyToManyField(TagGroup)
-
-class CallDetailEvent(models.Model):
+class Event(models.Model):
 
     #Always store as UTC naive
     start = models.DateTimeField(null=True, blank=True, db_index=True) # Also seen as event time
@@ -49,43 +37,95 @@ class CallDetailEvent(models.Model):
     event_type = models.CharField(default='CDR', max_length=45) #Default to CDR
     event_type_user_defined = models.CharField(max_length=45, null=True, blank=True)
 
-    callerid = models.ForeignKey(CallerID)
+    callerid_name = models.CharField(max_length=80)
+    callerid_number = models.CharField(max_length=80)
+    callerid_ani = models.CharField(max_length=80)
+    callerid_rdnis = models.CharField(max_length=80)
+    callerid_dnid = models.CharField(max_length=80)
+
+    callerid_presentation_name = models.CharField(max_length=80)
+    callerid_presentation_number = models.CharField(max_length=80)
 
     source = models.CharField(max_length=160, null=True, blank=True)
     destination = models.CharField(max_length=160, null=True, blank=True) #Also seen as extension for event data
 
+    sequence = models.IntegerField(null=True, blank=True)
+
     context = models.CharField(max_length=80, null=True, blank=True)
 
     channel = models.CharField(max_length=240, null=True, blank=True) #Also seen as channname for event data
-    channel_tech = models.SmallIntegerField(null=True, blank=True)
+    channel_tech = models.SmallIntegerField(null=True, blank=True) #Index with channel
     channel_counter = models.IntegerField() #Index with channel
 
     channel_destination = models.CharField(max_length=240, null=True, blank=True)
-    channel_destination_tech = models.SmallIntegerField(null=True, blank=True)
+    channel_destination_tech = models.SmallIntegerField(null=True, blank=True) #Index with channel_destionation
     channel_destination_counter = models.IntegerField() #Index with channel_destination
 
     application = models.CharField(max_length=80, null=True, blank=True)
-    application_data = models.CharField(max_length=160, null=True, blank=True)
+    application_data = models.CharField(max_length=80, null=True, blank=True)
 
-    duration_call = models.IntegerField()
+    duration = models.IntegerField()
     duration_billed = models.IntegerField()
 
-    disposition = models.CharField(max_length=45, null=True, blank=True)
+    disposition = models.SmallIntegerField(default=1)
 
     ama_flags = models.IntegerField()
 
-    account_code = models.CharField(max_length=160, null=True, blank=True)
-    account_code_peer = models.CharField(max_length=160, null=True, blank=True)
+    account_code = models.CharField(max_length=20, null=True, blank=True)
+    account_code_peer = models.CharField(max_length=20, null=True, blank=True)
 
-    user_field = models.CharField(max_length=255, null=True, blank=True)
+    user_field = models.CharField(max_length=256, null=True, blank=True)
 
-    unique_id = models.CharField(max_length=255, null=True, blank=True)
-    linked_id = models.CharField(max_length=255, null=True, blank=True)
+    unique_id = models.CharField(max_length=150, null=True, blank=True)
+    linked_id = models.CharField(max_length=150, null=True, blank=True)
 
     peer = models.CharField(max_length=80, null=True, blank=True)
 
     origin_id = models.IntegerField(null=True, blank=True, db_index=True)
-    origin_sync_key = models.CharField(max_length=255, null=True, blank=True)
+    origin_sync_key = models.CharField(max_length=1024, null=True, blank=True)
 
     batch = models.ForeignKey(Batch)    
     tag_hash = models.ForeignKey(TagHash)
+
+    custom_integer_01 = models.IntegerField(null=True, blank=True)
+    custom_integer_02 = models.IntegerField(null=True, blank=True)
+    custom_integer_03 = models.IntegerField(null=True, blank=True)
+    custom_integer_04 = models.IntegerField(null=True, blank=True)
+    custom_integer_05 = models.IntegerField(null=True, blank=True)
+    custom_integer_06 = models.IntegerField(null=True, blank=True)
+    custom_integer_07 = models.IntegerField(null=True, blank=True)
+    custom_integer_08 = models.IntegerField(null=True, blank=True)
+    custom_integer_09 = models.IntegerField(null=True, blank=True)
+    custom_integer_10 = models.IntegerField(null=True, blank=True)
+    custom_integer_11 = models.IntegerField(null=True, blank=True)
+    custom_integer_12 = models.IntegerField(null=True, blank=True)
+    custom_integer_13 = models.IntegerField(null=True, blank=True)
+    custom_integer_14 = models.IntegerField(null=True, blank=True)
+    custom_integer_15 = models.IntegerField(null=True, blank=True)
+    custom_integer_16 = models.IntegerField(null=True, blank=True)
+    custom_integer_17 = models.IntegerField(null=True, blank=True)
+    custom_integer_18 = models.IntegerField(null=True, blank=True)
+    custom_integer_19 = models.IntegerField(null=True, blank=True)
+    custom_integer_20 = models.IntegerField(null=True, blank=True)
+
+    custom_string_01 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_02 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_03 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_04 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_05 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_06 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_07 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_08 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_09 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_10 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_11 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_12 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_13 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_14 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_15 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_16 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_17 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_18 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_19 = models.CharField(max_length=1024, null=True, blank=True)
+    custom_string_20 = models.CharField(max_length=1024, null=True, blank=True)
+
